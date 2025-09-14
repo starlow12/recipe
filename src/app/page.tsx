cd5ba34b46a9e244e939c14959ec612b54c58a06
starlow12 <4starlow@gmail.com>
2025-09-14 19:42:51 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '20px', textAlign: 'center'}}>
+      <h1>RecipeGram</h1>
+      <p>Welcome to our recipe sharing platform!</p>
+    </main>
+  )
+}
